d38cd9027f74f5a20f79f3c7b3adb6d0b6ec89af
FRoGFraGy <frogdev05@gmail.com>
2026-06-24 12:06:43 +0700

    <py> fix up02 file

--- Week1/up02_thread.py
+++ Week1/up02_thread.py
@@ -2,18 +2,15 @@ from time import sleep, ctime, time
 import threading
 
 def update_cup_number(customer_name):
-    print(f"{ctime()} | Making coffee for {customer_name}...")
-    sleep(1)
-    print(f"{ctime()} | Coffee ready for {customer_name}!")
-
-def update_lcd(customer_name):
     print(f"{ctime()} | LCD: Processing for customer {customer_name}...")
     sleep(1)
     print(f"{ctime()} | LCD: Done for customer {customer_name}.")
 
 def make_coffee(customer_name):
+    print(f"{ctime()} | Making coffee for {customer_name}...")
+    sleep(1)
+    print(f"{ctime()} | Coffee ready for {customer_name}!")
     update_cup_number(customer_name)
-    update_lcd(customer_name)
 
 def main():
     print(f"{ctime()} | === Multi-threading Coffee Machine ===")
