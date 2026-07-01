40b957fca29f4b9b7e7f644f8ee1dde4fbb483fd
FRoGFraGy <frogdev05@gmail.com>
2026-07-01 10:16:51 +0700

    <py> edit asycio06 file

--- Week2/asyncio06.py
+++ Week2/asyncio06.py
@@ -1,2 +1,24 @@
 # Program 6: Creating a Concurrent Task
 # Concept: Wrapping a coroutine inside asyncio.create_task() to schedule it to run in the background.
+import asyncio
+from time import time, ctime
+
+async def cook_spaghetti(customer):
+    print(f"{ctime()} -> Starting cooking for Customer {customer}...")
+    await asyncio.sleep(1)  # Simulating a time-consuming task
+    print(f"{ctime()} -> Finished cooking for Customer {customer}!")
+
+async def main():
+    start = time()
+
+    # Creating tasks for each customer
+    task_a = asyncio.create_task(cook_spaghetti("A"))
+
+    print(f"{ctime()} -> Main program can do other thing while Task A runs in the background.")
+
+    await task_a  # Awaiting the completion of Task A
+
+    print(f"Total Operation Time: {time() - start:.2f} seconds")
+
+if __name__ == "__main__":
+    asyncio.run(main())  # This will execute the main coroutine and print the total time taken to serve the customer.
