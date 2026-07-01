78877ddcc7aa43005c9a5e67325fee6a683e5650
FRoGFraGy <frogdev05@gmail.com>
2026-07-01 09:29:11 +0700

    <py> edit asycio01 file

--- Week2/asyncio01.py
+++ Week2/asyncio01.py
@@ -1,2 +1,11 @@
 # Program 1: The First Coroutine Function
 # Concept: Understanding async def and how it differs from a normal function.
+import asyncio
+
+#A regular function uses def An async function uses async def.
+#This defines a coroutine function.
+
+async def great():
+    print("Hello from Coroutine!")
+
+print(type(great))  #output <class 'function'>
