986709b73297f5ae4433dc055b15cce5b106d99e
FRoGFraGy <frogdev05@gmail.com>
2026-07-01 09:32:25 +0700

    <py> edit asycio02,03 file

--- Week2/asyncio02.py
+++ Week2/asyncio02.py
@@ -1,2 +1,12 @@
 # Program 2: The Coroutine Object
 # Concept: Seeing that calling an async def function creates an "Object" but does not execute it yet.
+import asyncio
+
+async def great():
+    print("Hello!")
+
+coro_object = great()  # This creates a coroutine object but does not execute it yet.
+
+print(type(coro_object))  #output <class 'coroutine'>
+
+coro_object  # This will execute the coroutine and print "Hello!"
--- Week2/asyncio03.py
+++ Week2/asyncio03.py
@@ -1,2 +1,11 @@
 # Program 3: The Event Loop (asyncio.run)
 # Concept: Using the Event Loop to actually execute a Coroutine Object.
+import asyncio
+
+async def great():
+    print("Hello from the Event Loop!")
+
+if __name__ == "__main__":
+    coro_object = great()  # This creates a coroutine object but does not execute it yet.
+
+    asyncio.run(coro_object)  # This will execute the coroutine and print "Hello from the Event Loop!"
