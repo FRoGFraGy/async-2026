46a9cb807ec92480bd9d2ce69da9d7aa5e1801c6
FRoGFraGy <frogdev05@gmail.com>
2026-07-15 09:40:27 +0700

    <py> edit foodcourt_02 file

--- Week4/foodcourt_02_gather.py
+++ Week4/foodcourt_02_gather.py
@@ -1,1 +1,25 @@
 # foodcourt_02_gather.py
+import asyncio
+from time import ctime
+from food_utils import send_order_to_kitchen
+
+async def main():
+    MY_STUDENT_ID = "65010001"
+    print(f"{ctime()} | --- [Task 2] Practice using gather to queue multiple orders ---")
+    
+    # 1. Create a list of tasks for ordering different food items.
+    tasks = [
+        send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice Mixed"),
+        send_order_to_kitchen(MY_STUDENT_ID, "noodle", "noodle"),
+        send_order_to_kitchen(MY_STUDENT_ID, "steak", "steak")
+    ]
+    
+    # 2. Use asyncio.gather to run all tasks concurrently and wait for their completion.
+    results = await asyncio.gather(*tasks)
+    
+    # 3. Print the results of all orders once they are completed.
+    for result in results:
+        print(f"{ctime()} | [{result['status']}] Shop: '{result['shop']}' | '{result['menu']}'")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- Week4/foodcourt_03_wait_first.py
+++ Week4/foodcourt_03_wait_first.py
@@ -1,1 +1,31 @@
 # foodcourt_03_wait_first.py
+import asyncio
+from asyncio import tasks
+from time import ctime
+from food_utils import send_order_to_kitchen
+
+async def main():
+    MY_STUDENT_ID = "65010001"
+    print(f"{ctime()} | --- [Task 3] Practice using wait to queue multiple orders ---")
+    
+    # 1. Create a list of tasks for ordering different food items.
+    tasks = [
+    asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice Mixed")),
+    asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "pad_thai", "Pad Thai")),
+    asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "sushi", "Sushi Platter"))
+    ]
+    
+    # 2. Use asyncio.wait to run all tasks concurrently and wait for their completion.
+    done, pending = await asyncio.wait(
+        tasks,
+        return_when=asyncio.FIRST_COMPLETED
+    )    
+    # 3. Print the results of the completed orders.
+    print(f"{ctime()} | Canceling {len(pending)} pending order(s)...")
+    for task in pending:
+        task.cancel()
+
+    await asyncio.gather(*pending, return_exceptions=True)
+
+if __name__ == "__main__":
+    asyncio.run(main())
